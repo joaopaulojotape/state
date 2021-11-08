ff46328d71e8e56d447d2b9ea002a80b0ddbdde7
João Paulo <79548287+jotapeee@users.noreply.github.com>
2021-11-08 10:42:11 -0300

    Add

--- src/test/java/CarroStateTest.java
+++ src/test/java/CarroStateTest.java
@@ -1,22 +1,43 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 
 public class CarroStateTest {
-
     @Test
     public void teste(){
         Carro carro = new Carro();
-        carro.potencia();
-        carro.seta();
-        carro.desligar();
-        carro.ligar();
-        carro.ligar();
-        carro.desligar();
-        carro.desligar();
-        carro.seta();
-        carro.desligar();
-        carro.desligar();
-        carro.desligar();
+//        carro.potencia();
+//        carro.seta();
+//        carro.desligar();
+//        carro.ligar();
+//        carro.ligar();
+//        carro.desligar();
+//        carro.desligar();
+//        carro.seta();
+//        carro.desligar();
+//        carro.desligar();
+//        carro.desligar();
+
+        assertEquals("Mais potência", carro.estado.ativarTurbo());
+
+
     }
-}
+
+    @Test
+    public void testeSeta(){
+        Carro carro = new Carro();
+
+        carro.seta();
+        assertEquals("seta", carro.estado.seta());
+
+    }
+
+
+@Test
+public void testeDesligar(){
+        Carro carro = new Carro();
+        assertEquals("Desligado",carro.estado.desligarCarro() );
+
+        }
+ }
 
